97bff657f0bf5fdfac5a286f8aa7b5148275e69a
Wise <Wisedemic@users.noreply.github.com>
2018-04-14 13:36:50 -0400

    Delete config.js

--- api/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  secret: 'muahahahahhahaha123',
-  mongoURL: 'mongodb://wisedemic:bednblockisking@bednblockchain-shard-00-00-vvrrv.mongodb.net:27017,bednblockchain-shard-00-01-vvrrv.mongodb.net:27017,bednblockchain-shard-00-02-vvrrv.mongodb.net:27017/test?ssl=true&replicaSet=BedNBlockchain-shard-0&authSource=admin'
-}
