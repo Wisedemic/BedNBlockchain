0c9b1760db6f51f44accfcece2f1f450c07b1e17
Wisedemic <navarrete.tristan@gmail.com>
2018-04-14 13:32:47 -0400

    MongooseDB Error handling and config file updates.

--- api/config/config.js
+++ api/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret: 'muahahahahhahaha123',
-  mongoURL: ''
+  mongoURL: 'mongodb://wisedemic:bednblockisking@bednblockchain-shard-00-00-vvrrv.mongodb.net:27017,bednblockchain-shard-00-01-vvrrv.mongodb.net:27017,bednblockchain-shard-00-02-vvrrv.mongodb.net:27017/test?ssl=true&replicaSet=BedNBlockchain-shard-0&authSource=admin'
 }
--- api/config/database.js
+++ api/config/database.js
@@ -6,19 +6,19 @@ const config = require('./config.js');
 mongoose.Model.MongoErrors = function(errs) {
 	var data = [];
 	var counter = 0;
-	for (var key in errs.errors) {
-		console.log(errs.errors[key]);
-	    data[counter] = {el: '#'+errs.errors[key].path, error: errs.errors[key].properties.message};
+	for (var label in errs.errors) {
+		console.log(errs)
+			const msg = errs.errors[label].properties.message;
+	    data[counter] = {label: msg};
 		counter++;
 	}
-	console.log(data);
 	return data;
 }
 
 // Connect to MongoDB
 mongoose.connect(config.mongoURL, function (err, res) {
     if (err) {
-        console.log('[DB] Connection to "' + config.mongoURL + '" failed!. ' + err);
+        console.log('[DB] Connection to MongoDB failed!. ' + err);
     } else {
         console.log('[DB] Successfully connected to MongoDB Server');
     }
