a8ead5973142ce0eb7678e71a850ab595674376d
Wisedemic <navarrete.tristan@gmail.com>
2018-04-20 10:41:52 -0400

    Added the option for a label

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 import BulmaInput from './Input';
+
+const Label = props => {
+  if (props.label.length > 0 ) {
+    return (
+      <label className="label">{props.label}</label>
+    );
+  } else {
+    return null;
+  }
+}
+
 const Message = props => {
   if (props.message.length > 0) {
     return (
@@ -17,6 +28,7 @@ class BulmaField extends Component {
   render() {
     return (
       <div className="field">
+        <Label label={this.props.label}/>
         <p className={'control'}>
           <BulmaInput
             onChange={this.props.onChange}
