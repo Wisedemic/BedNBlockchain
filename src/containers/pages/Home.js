f6f2c245e95c6ac705ecabc277b4f46537316bb1
Tristan Navarrete <navarrete.tristan@gmail.com>
2018-04-22 15:11:50 -0400

    Homepage now has a featured rooms section.

--- src/containers/pages/Home.js
+++ src/containers/pages/Home.js
@@ -33,31 +33,45 @@ class Home extends Component {
   render() {
 		console.log(this.props);
     return (
-      <section id="home" className="hero is-light is-fullheight">
-				<div className="banner" style={{backgroundImage: 'url('+Banner+')'}}></div>
-				<div className="hero-body">
-					<div className="container has-text-centered">
-	          <h1 className="title is-1">Find a Room, or List One</h1>
-						<h4 className="subtitle is-4">{this.props.appName} does it all!</h4>
-						<div className="columns is-centered">
-							<div className="column is-narrow">
-								<div className="field has-addons">
-								  <p className="control">
-								    <Link className="button is-info is-outlined is-medium" to="/rooms">
-								      <span>Book Now</span>
-								    </Link>
-								  </p>
-								  <p className="control">
-								    <Link className="button is-danger is-outlined is-medium" to={(this.props.token > 16) ? '/your-rooms/add' : '/signup'}>
-								      <span>List A Property</span>
-								    </Link>
-								  </p>
+			<div>
+	      <section id="home" className="hero is-light is-fullheight">
+					<div className="banner" style={{backgroundImage: 'url('+Banner+')'}}></div>
+					<div className="hero-body">
+						<div className="container has-text-centered">
+		          <h1 className="title is-1">Find a Room, or List One</h1>
+							<h4 className="subtitle is-4">{this.props.appName} does it all!</h4>
+							<div className="columns is-centered">
+								<div className="column is-narrow">
+									<div className="field has-addons">
+									  <p className="control">
+									    <Link className="button is-info is-outlined is-medium" to="/rooms">
+									      <span>Book A Now</span>
+									    </Link>
+									  </p>
+									  <p className="control">
+									    <Link className="button is-danger is-outlined is-medium" to={(this.props.token > 16) ? '/your-rooms/add' : '/signup'}>
+									      <span>List A Property</span>
+									    </Link>
+									  </p>
+									</div>
 								</div>
 							</div>
 						</div>
+	        </div>
+	      </section>
+				<section id="featured" className="hero is-fullheight">
+					<div className="hero-body">
+						<div className="container has-text-centered">
+							<h2 className="title is-2">Featured Rooms</h2>
+							<div className="columns">
+								<div className="column">Room #1</div>
+								<div className="column">Room #2</div>
+								<div className="column">Room #3</div>
+							</div>
+						</div>
 					</div>
-        </div>
-      </section>
+				</section>
+			</div>
     );
   }
 }
