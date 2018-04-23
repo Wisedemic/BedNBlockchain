f01ccc8953e1790e727b72062cc221f2dc80c8df
Wisedemic <navarrete.tristan@gmail.com>
2018-04-22 23:05:04 -0400

    Updated 404 with Return Home button

--- src/components/404.js
+++ src/components/404.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Four_Oh_Four extends React.Component {
   componentWillMount() {}
   componentWillUnmount() {}
   render() {
     return (
-      <section id="404" className="hero is-light is-bold is-fullheight">
+      <section id="404" className="hero is-dark is-bold is-fullheight">
         <div className="hero-body">
-          <h1 className="title is-1">Uh oh! This page was not found!</h1>
+          <div className="container has-text-centered">
+            <h1 className="title is-1">Uh oh! This page was not found!</h1>
+            <br />
+            <Link className="button is-info is-large" to="/">
+              Return Home
+            </Link>
+          </div>
         </div>
       </section>
     );
