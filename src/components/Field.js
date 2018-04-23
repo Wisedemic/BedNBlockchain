1874e6fd5ac552cd940a885c5e1e65133a24a44b
Wisedemic <navarrete.tristan@gmail.com>
2018-04-23 02:28:42 -0400

    Field Component now supports addons, icons, and a horizontal layout as well.

--- src/components/Field.js
+++ src/components/Field.js
@@ -6,7 +6,7 @@ const Label = props => {
 	if (!props.label) return null;
   if (props.label.length > 0 ) {
     return (
-      <label className="label">{props.label}</label>
+      <label className={props.className}>{props.label}</label>
     );
 	}
 	return null;
@@ -22,43 +22,76 @@ const Message = props => {
   }
 };
 
+const FieldWrapper = props => {
+	if (props.isHorizontal) {
+		return (
+			<div className={'field is-horizontal'}>
+				{props.label ? (<Label label={props.label} className="field-label is-normal" />) : null}
+				<div className="field-body">
+					<div className={'field is-expanded'}>
+						<div className={'field' + (props.hasAddonLeft || props.hasAddonRight ? ' has-addons' : '')}>
+							{props.children}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className={'field' + (props.hasAddonLeft || props.hasAddonRight ? ' has-addons' : '')}>
+				{props.label ? (<Label label={props.label} className="label" />) : null}
+				{props.children}
+			</div>
+		);
+	}
+}
 
 class BulmaField extends Component {
 
   render() {
-    return (
-      <div className="field">
-        <Label label={this.props.label}/>
-
-				{this.props.type === 'select' ? (
-					<div className={'control'}>
-	          <BulmaInput
-	            onChange={this.props.onChange}
-							opts={this.props.opts}
-	            type={this.props.type}
-	            placeholder={this.props.placeholder}
-	            value={this.props.value}
-	            disabled={this.props.disabled}
-	            inputState={this.props.inputState}
-	          />
+		return (
+			<FieldWrapper
+				label={this.props.label ? this.props.label : null}
+				hasAddonLeft={this.props.hasAddonLeft}
+				hasAddonRight={this.props.hasAddonRight}
+				isHorizontal={this.props.isHorizontal}
+				inputState={this.props.inputState}
+				message={this.props.message}
+			>
+				{this.props.hasAddonLeft ? (
+					<div className="control">
+						{this.props.hasAddonLeft}
+					</div>
+				) : null}
+				<div className={'control' + (this.props.hasIconRight ? ' has-icons-right' : '') + (this.props.hasIconLeft ? ' has-icons-left' : '')}>
+					<BulmaInput
+						onChange={this.props.onChange}
+						opts={this.props.opts}
+						type={this.props.type}
+						placeholder={this.props.placeholder}
+						value={this.props.value}
+						disabled={this.props.disabled}
+						inputState={this.props.inputState}
+					/>
+					{this.props.hasIconLeft ? (
+						<span className={'icon is-left'}>
+							<i className={'fa fa-' + this.props.hasIconLeft}></i>
+						</span>
+					) : null}
+					{this.props.hasIconRight ? (
+						<span className={'icon is-right'}>
+							<i className={'fa fa-' + this.props.hasIconRight}></i>
+						</span>
+					) : null}
+					<Message inputState={this.props.inputState} message={this.props.message}/>
 				</div>
-				) :
-				(
-					<p className={'control'}>
-	          <BulmaInput
-	            onChange={this.props.onChange}
-							opts={this.props.opts}
-	            type={this.props.type}
-	            placeholder={this.props.placeholder}
-	            value={this.props.value}
-	            disabled={this.props.disabled}
-	            inputState={this.props.inputState}
-	          />
-	        </p>
-				)
-				}
-        <Message inputState={this.props.inputState} message={this.props.message}/>
-      </div>
+				{this.props.hasAddonRight ? (
+					<p className="control">
+						{this.props.hasAddonRight}
+					</p>
+				) : null}
+
+			</FieldWrapper>
     );
   }
 }
