0e68d6d0bf8f58dc9a45118259cfb529139d489f
Tristan Navarrete <navarrete.tristan@gmail.com>
2018-04-25 15:53:31 -0400

    Rooms page now displays rooms in a flex grid.

--- src/containers/pages/Rooms.js
+++ src/containers/pages/Rooms.js
@@ -36,12 +36,22 @@ class Rooms extends Component {
 				<div className="hero-body">
 					<div className="container has-text-centered">
 						<h2 className="title is-2">Browse Rooms</h2>
-							<div className="box">
+							<div id="rooms">
 								{this.props.roomsList ? (
 									this.props.roomsList.map((room, index) => {
 										return (
-											<div className="box" key={index}>
-												<code>{room.id}</code>
+											<div className="card room" key={index}>
+												<div class="card-image">
+											    <figure class="image is-4by3">
+											      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+											    </figure>
+											  </div>
+												<div class="card-content">
+													<div class="content">
+														<h4 className="title is-4">{room.title}</h4>
+														{room.description}
+											    </div>
+												</div>
 											</div>
 										);
 									}, this)
