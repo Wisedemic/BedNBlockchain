ce803b7668e1a5fa37bdc23e81dee79d593f590b
Wisedemic <navarrete.tristan@gmail.com>
2018-05-09 13:13:33 -0400

    File cleanup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
-import auth from './auth';
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import auth from './auth';
 import common from './common';
 import home from './home';
 import rooms from './rooms';
@@ -7,7 +8,6 @@ import roomEditor from './roomEditor';
 import bookings from './bookings';
 // import profile from './profile';
 // import settings from './settings';
-import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
   auth,
