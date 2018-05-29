d6bdc533cbf9a632316e6096894746c41d0e731d
Wisedemic <navarrete.tristan@gmail.com>
2018-05-29 09:05:25 -0400

    Nicer Console.logging

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import { createServer } from 'http';
 import api from './server';
 import { PORT } from './constants';
 console.log('[SERVER] Bed\'N\'Blockchain API starting....');
-const SERVER_START = `[SERVER] Bed\'N\'Blockchain API sucessfully started on ${PORT}!`;
+const SERVER_START = `[SERVER] Bed\'N\'Blockchain API sucessfully started on ${PORT}! That took`;
 console.time(SERVER_START);
 const server = createServer(api);
 server.listen(PORT, () => console.timeEnd(SERVER_START));
