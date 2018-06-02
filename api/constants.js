90caedefcdc4160a6d7736c1d6fe40fe0c165a8e
Wisedemic <navarrete.tristan@gmail.com>
2018-06-02 04:18:18 -0400

    Testing Heroku environement variable solution

--- api/constants.js
+++ api/constants.js
@@ -1,10 +1,18 @@
 import { cleanEnv, num, str } from 'envalid';
+const options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.dotEnvPath = null;
+  options.reporter = ({ errors = {}, env = {} }) => {
+    console.log(errors, env);
+  };
+}
 
 const env = cleanEnv(process.env, {
   PORT: num({ default: process.env.PORT || 3000 }),
   MONGO_DB_URI: str(),
   SECRET: str()
-});
+}, options);
 
 export const { PORT, MONGO_DB_URI, SECRET } = env;
 
--- client/constants.js
+++ client/constants.js
@@ -1,9 +1,18 @@
 /* This is used to pass down env vars from the server into react. */
 import { cleanEnv, str } from 'envalid';
 
+const options = {};
+
+if (process.env.NODE_ENV === 'production') {
+  options.dotEnvPath = null;
+  options.reporter = ({ errors = {}, env = {} }) => {
+    console.log(errors, env);
+  };
+}
+
 const env = cleanEnv(process.env, {
   VERSION: str({ default: '5.0.0' }),
-});
+}, options);
 
 export const { VERSION } = env;
 
